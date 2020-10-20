64a6e9fc6c3a259a5ea1fd5f9a0672a7556e451b
vikkitikky <vikkitikky@gmail.com>
2020-10-20 12:47:30 +0300

    add initial cards to document

--- scripts/script.js
+++ scripts/script.js
@@ -28,3 +28,47 @@ function formSubmitHandler (evt) {
 editBtn.addEventListener('click', formVisibility);
 closeBtn.addEventListener('click', formHidden);
 form.addEventListener('submit', formSubmitHandler);
+
+//карточки из коробки
+
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+const photoContainer = document.querySelector('.photo');
+
+function addCard(name, link) {
+  const cardTemlplate = document.querySelector('#photo-template').content;
+  const cardElement = cardTemlplate.cloneNode(true);
+
+  cardElement.querySelector('.element__title').textContent = name;
+  cardElement.querySelector('.element__img').src = link;
+  cardElement.querySelector('.element__img').alt = name;
+  photoContainer.prepend(cardElement);
+}
+
+initialCards.forEach(item => addCard(item.name, item.link))
+
