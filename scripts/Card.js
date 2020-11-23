40415e61e44343ca455ac8b96154ebfefcd0c96a
vikki_tikky <vikki.tikky@gmail.com>
2020-11-23 23:24:33 +0300

    исправления index.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,3 +1,10 @@
+import {
+  popupWithImage,
+  popupCaption,
+  popupImage
+} from "./constants.js";
+import {openPopup} from "./utils.js";
+
 export class Card {
   constructor(data, templateSelector) {
     this._templateSelector = templateSelector;
@@ -23,21 +30,34 @@ export class Card {
     element.remove();
   }
 
-  _setEventListener (element, likeBtn, deleteBtn) {
+  _setDataForBigImage() {
+    popupImage.src = this._image;
+    popupImage.alt = this._title;
+    popupCaption.textContent = this._title;
+  }
+
+  _openBigCard() {
+    this._setDataForBigImage();
+    openPopup(popupWithImage);
+  }
+
+  _setEventListener (element, likeBtn, deleteBtn, card) {
     likeBtn.addEventListener('click', () => this._setLike(likeBtn));
     deleteBtn.addEventListener('click', () => this._deleteCard(element));
+    card.addEventListener('click', () => this._openBigCard());
   }
 
   generateCard() {
     this._element = this._getTemplate();
     this._likeButton = this._element.querySelector('.element__like-btn');
     this._deleteButton = this._element.querySelector('.element__delete-btn');
+    this._card = this._element.querySelector('.element__img');
 
     this._element.querySelector('.element__title').textContent = this._title;
     this._element.querySelector('.element__img').alt = this._title;
     this._element.querySelector('.element__img').src = this._image;
 
-    this._setEventListener(this._element, this._likeButton, this._deleteButton);
+    this._setEventListener(this._element, this._likeButton, this._deleteButton, this._card);
 
     return this._element;
   }
---
+++ scripts/constants.js
@@ -0,0 +1,37 @@
+export const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+export const popupWithImage = document.querySelector('.popup_type_card-view');
+export const popupImage = popupWithImage.querySelector('.popup__image');
+export const popupCaption = popupWithImage.querySelector('.popup__card-title');
+export default {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-btn',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+}
